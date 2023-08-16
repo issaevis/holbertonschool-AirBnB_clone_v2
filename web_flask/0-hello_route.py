ec334c425726fe0ebe3feae557aca9c0e8022a3f
issaevis <evis.topollaj@gmail.com>
2023-08-16 17:55:59 +0200

    Added 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+'''module for the first flask program'''
 
 
 from flask import Flask
-'''module for the first flask program'''
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
     return "<p>Hello HBNB!</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
