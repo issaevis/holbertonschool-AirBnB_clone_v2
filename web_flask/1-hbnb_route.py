d83e2e2ac70107f1f4bf33fa24865255be4a0789
issaevis <evis.topollaj@gmail.com>
2023-08-16 18:11:36 +0200

    Added 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 '''module for flask program'''
-
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,6 +8,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hello():
