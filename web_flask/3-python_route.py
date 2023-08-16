1e521a0e1cb94dd82b78c8900e07f64da737f41a
issaevis <evis.topollaj@gmail.com>
2023-08-16 18:37:46 +0200

    Added 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''module for flask with variables and multole routes'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def variable(text):
+    ttext = text.replace('_', ' ')
+    return f"C {ttext}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def variable(text="is cool"):
+    ttext = text.replace('_', ' ')
+    return f"Python {ttext}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
