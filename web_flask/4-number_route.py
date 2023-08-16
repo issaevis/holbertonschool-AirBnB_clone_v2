3cef1712d33061e0b8ff41baa1fe12ea2839afd9
issaevis <evis.topollaj@gmail.com>
2023-08-16 18:54:16 +0200

    Added 5

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,10 +30,9 @@ def variable_pyth(text="is cool"):
 
 
 @app.route('/number/')
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def is_n(n):
-    if n == int:
-        return f"{n} is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
