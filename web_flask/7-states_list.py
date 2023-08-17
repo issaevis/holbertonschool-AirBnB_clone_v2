f5363dd20024d8501176a508e4fb47a3ba5a2d57
issaevis <evis.topollaj@gmail.com>
2023-08-17 12:29:43 +0200

    Added 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''linking flask with the website!!'''
+from flask import Flask
+from models import storage
+from models.state import State
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def root_route():
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close(self):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
