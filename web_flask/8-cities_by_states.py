f37517ddbfcc8474465d53140c739af323312950
issaevis <evis.topollaj@gmail.com>
2023-08-26 15:12:59 +0200

    Added a fix to 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,17 +4,20 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def root_route():
+def list_states_cities():
     cities_by_state = {}
     states = storage.all("State").values()
     for state in states:
         cities_by_state[state.name] = state.cities
-    return render_template("8-cities_by_states.html", states=states, cities=cities_by_state)
+    return render_template("8-cities_by_states.html",
+                           states=states, cities=cities_by_state)
 
 
 @app.teardown_appcontext
